e81380ef7aa39d61436c8977d0b8599950781fce
KSokhal <52101915+KSokhal@users.noreply.github.com>
2021-06-10 20:07:57 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../styles/theme';
-import Navbar from "../components/Navbar"
 
 
 export default function MyApp(props) {
@@ -27,7 +26,6 @@ export default function MyApp(props) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <Navbar />
         <Component {...pageProps} />
       </ThemeProvider>
     </React.Fragment>
